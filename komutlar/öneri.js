81a117a54d4a74e47c69b3dc4bad99a441a841cb
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-07-08 18:15:56 +0300

    Update öneri.js

--- komutlar/öneri.js
+++ komutlar/öneri.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
         //kod başlangıç
 
         let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-        if(!User) return message.reply(":warning: `Lütfen Şikayet İstediğin Kullanıcınında İsmini Yaz`");
+        if(!User) return message.reply(":warning: `Kullanım : l!öner @[öneren kişi] [önerin]`");
         let öneri = args.join(" ").slice(22);
         if(!öneri) return message.reply(":warning: `Kullanım : l!öner @[öneren kişi] [önerin]`")
 
