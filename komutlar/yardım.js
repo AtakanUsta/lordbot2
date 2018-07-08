b6a896ba6b81c4e7d82b5b1c8fb7678f4bc617e0
LordMusoTR <39191382+LordMusoTR@users.noreply.github.com>
2018-07-08 20:48:41 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message, args) => {
             .setColor("#00ff08")
             .addField("Admin Komutları", `✅ **${botconfig.prefix}yaz [mesaj]** | Bota Mesaj Yazdırmayı Sağlar \n\n✅ **${botconfig.prefix}temizle [miktar]** | Seçtiğiniz Miktarda Mesajı Silmeye Yarar\n\n✅ **${botconfig.prefix}sustur @[kullanıcı] [milisaniye]** | Kişiyi Belli Milisaniye Susturur \n               **NOT : 1 Saniye = 1000milisaniye**\n\n✅ **${botconfig.prefix}uyar @[kullanıcı] [sebep]** | Kullanıcıları Uyarmayı Sağlar (3 Uyarıda Kick)`)
             .addBlankField()
-            .addField("Kullanıcı Komutları", `✅ **${botconfig.prefix}bilgi @[kullanıcı]** | Kullanıcınin Bilgilerine Bakarsınız \n\n✅ **${botconfig.prefix}botkurucu** | LordBotun Kimin Tarafından Yapıldığına Bakarsınız \n\n✅ **${botconfig.prefix}uyarılar @[kullanıcı]** | Kullanıcının Kaç Uyarısı Olduğuna Bakarsınız \n\n✅ **${botconfig.prefix}gerisayım** | Bir Gerisayım Başlatırsınız`)
+            .addField("Kullanıcı Komutları", `✅ **${botconfig.prefix}bilgi @[kullanıcı]** | Kullanıcınin Bilgilerine Bakarsınız \n\n✅ **${botconfig.prefix}botkurucu** | LordBotun Kimin Tarafından Yapıldığına Bakarsınız \n\n✅ **${botconfig.prefix}uyarılar @[kullanıcı]** | Kullanıcının Kaç Uyarısı Olduğuna Bakarsınız \n\n✅ **${botconfig.prefix}gerisayım** | Bir Gerisayım Başlatırsınız\n\n✅ **${botconfig.prefix}avatar @[kullanıcı]** | Kullanıcının Avatarına Bakarsın`)
             .addBlankField()
             .addField("Destek Sunucumuza Gelmek Link'e Tıklayın", "**https://discord.gg/eaPEC8f**")
             .addBlankField()
